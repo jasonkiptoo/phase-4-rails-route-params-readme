a78e7e3eaaf8ec690573a084166549f9463d7416
JasonKiptoo <kiptoojason@gmail.com>
2022-11-30 01:55:34 +0300

    completed assignmnent

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  get '/cheeses', to: 'cheeses#index'
+  # get '/cheeses', to: 'cheeses#show'
+
+  # get '/cheeses/#{cheese.id}', to: 'cheeses#show'
+  get '/cheeses/:id', to: 'cheeses#show'
 end
